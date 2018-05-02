29f6540902deb6b46b86d77c0b8a384675617550
Amar Chatman <amarjerome@gmail.com>
2018-05-02 21:54:42 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,6 +2,7 @@ class Author
 
 attr_accessor :author, :name
 
+@@post_count = 0 
 
 def initialize(name)
   @name  = name
@@ -17,6 +18,7 @@ end
 def add_post(post)
   @posts << post
   post.author = self
+  @@post_count += 1
 end
 
 def add_post_by_title(name)
